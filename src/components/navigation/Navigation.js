813ff837106e632dd8c7c797363df68017e60042
cmarkymark <31522432+cmarkymark@users.noreply.github.com>
2020-01-12 14:14:58 -0800

    Update Navigation.js

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,11 +5,10 @@ import './Navigation.css';
 const Navigation = (props) => {
   return (
     <nav id="navigation">
-      <Dropdown heading={"Home"} />
-      <Dropdown heading={"Fascination"} drop={props.fascination} />
+      <Dropdown heading={"About"} />
       <Dropdown heading={"Research"} drop={props.research} />
       <Dropdown heading={"Teaching"} drop={props.teaching} />
-      <Dropdown heading={"About"} />
+      <Dropdown heading={"Fascination"} drop={props.fascination} />
     </nav>
   );
 };
