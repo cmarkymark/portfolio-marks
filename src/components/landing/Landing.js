e9732449640e1d89b3757dcf4598dd46f23c911d
cmarkymark <31522432+cmarkymark@users.noreply.github.com>
2020-01-16 10:53:39 -0800

    Update Landing.js

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -4,7 +4,9 @@ import './Landing.css';
 const Landing = ({landing}) => {
   return (
     <div id="landing">
-      This Website Is Currently Under Construction
+      Hello!  I (they/them) am a current doctoral student at SDSU and UCSD.  This site is largely under construction at the moment, but
+  I am hosting it as a space to share, and reflect on my research and teaching work, as well as to share extracurriculars that have been 
+  a requisite component of my academia self-care routine.  I will make this more formal in the coming months. -CM (1/16/20)
     </div>
   );
 };
