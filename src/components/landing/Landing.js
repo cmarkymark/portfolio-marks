6a5e145219e9e1c38d6384be89baaad8fbccf9d5
cmarkymark <31522432+cmarkymark@users.noreply.github.com>
2020-01-20 09:06:05 -0800

    Update Landing.js

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -6,7 +6,15 @@ const Landing = ({landing}) => {
     <div id="landing">
       Hello!  I (they/them) am a current doctoral student at SDSU and UCSD.  This site is largely under construction at the moment, but
   I am hosting it as a space to share, and reflect on my research and teaching work, as well as to share extracurriculars that have been 
-  a requisite component of my academia self-care routine.  I will make this more formal in the coming months. -CM (1/16/20)
+  a requisite component of my academia self-care routine.  
+  
+  Coming from both a social justice and a computer science background, I believe pretty firmly in open access, especially when it pertains both to knowledge
+  and the generation of knowledge.  On this site you will find resources related to my research as well as to the classes that I teach.
+  Hopefully, they can be of use to you in your own research, teaching, and education.
+  
+  Feel free to reach out.  I can be reached by email at cmarks (at) sdsu.edu 
+  
+  Charles Marks (1/20/20)
     </div>
   );
 };
