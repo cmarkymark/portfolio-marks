baef5b82c48d9cb7511ebf6d5fcb1cdbf976f86b
cmarkymark <31522432+cmarkymark@users.noreply.github.com>
2020-01-19 16:21:42 -0800

    Update Teaching.js

--- src/components/teaching/Teaching.js
+++ src/components/teaching/Teaching.js
@@ -17,7 +17,11 @@ const Teaching = (props) => {
   }
   return (
     <div id="teaching">
-      Teaching
+      Within this section of the website you will find course materials for classes I am teaching and have taught.  
+    There are many reasons I think it is important for instructors to make resources publicly available,
+    a primary one simply being that knowledge is collective. For those not taking my classes, whether a student, teacher, or
+    whatever, I hope the materials can be resources for your benefit. The experience of taking a class comes with support and communication
+    that these materials alone cannot convey, but I hope they may be helpful. 
       <div>
         {links}
       </div>
