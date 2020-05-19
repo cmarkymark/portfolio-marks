61962e0bf2b29f5dedbc1be0270a27036afe29a2
cmarkymark <31522432+cmarkymark@users.noreply.github.com>
2020-05-19 09:18:24 -0700

    Update Landing.js

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -12,14 +12,12 @@ const Landing = ({landing}) => {
     University and the University of California, San Diego. My dissertation research focuses on applying a trauma-informed lens to tobacco use
     research, with the intention of generalizing this trauma-informed approach for substance use research more generally. 
     <br/>
+      <br/>
     <img class="image" src="Pics/myface.jpg" alt=""/>
     
      </div>
   <div class="spacer"></div>
   <div class="column"> 
-    
-      <br/>
-      <br/>
     On this website you will find resources related to my research and teaching, as well as additional educational tools and 
     some extracurriculars! Under research you can find information about my research work, including access to R code used to run published analyses. Under
   teaching, there are tutorials (under construction) for running various statistical tests in R that are commonly used within the social sciences. 
