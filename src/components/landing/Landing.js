36ce8cf301bc8782f85a3d93b21a3ff574d4eb29
cmarkymark <31522432+cmarkymark@users.noreply.github.com>
2021-06-24 10:55:51 -0700

    Update Landing.js

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -5,27 +5,15 @@ const Landing = ({landing}) => {
   return (
    
     <div id="landing">
-  <div class="row">
-    <div class ="spacer"> </div>
-    <div class="column"> 
-    Hello! I (they/them) am a doctoral candidate in the Joint Doctoral Program in Interdisciplinary Research on Substance Use at San Diego State
-    University and the University of California, San Diego. My dissertation research focuses on applying a trauma-informed lens to tobacco use
-    research, with the intention of generalizing this trauma-informed approach for substance use research more generally. 
+  
+    Hello! I am Charles Marks (they/them), MPH, PhD. I am a subtance use researcher at the University of California San Diego and the University of Nevada, Reno. 
+  My research has focused on a wide range of topics including: cigarette use, injection drug use, opioid overdose. I apply a wide range of statistical and mathematical modeling
+  strategies within my research, but I am also quite critical of the application of quantitative methods within substance use epidemiology. I led the development of the Trauma-Informed
+  Theory of Individual Health Behavior -- the theory posits that people make their best effort to address the most immeidate harms they face and, thus, that providing individuals
+  and communities the resources and autonomy to address such harms is the best way to empower folx to live healthful lives.
     <br/>
       <br/>
     <img class="image" src="Pics/myface.jpg" alt=""/>
-    
-     </div>
-  <div class="spacer"></div>
-  <div class="column"> 
-    On this website you will find resources related to my research and teaching, as well as additional educational tools and 
-    some extracurriculars! Under research you can find information about my research work, including access to R code used to run published analyses. Under
-  teaching, there are tutorials (under construction) for running various statistical tests in R that are commonly used within the social sciences. 
-      </div>
-  <div class = "spacer"></div>
-</div>
-  
- 
     </div>
    
   );
