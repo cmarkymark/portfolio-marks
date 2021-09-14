059552f648fb7b4b87ba862b3eb212f8d14fc3b1
cmarkymark <31522432+cmarkymark@users.noreply.github.com>
2021-09-14 08:01:48 -0700

    Update Landing.js

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -11,9 +11,6 @@ const Landing = ({landing}) => {
   strategies within my research, but I am also quite critical of the application of quantitative methods within substance use epidemiology. I led the development of the Trauma-Informed
   Theory of Individual Health Behavior -- the theory posits that people make their best effort to address the most immeidate harms they face and, thus, that providing individuals
   and communities the resources and autonomy to address such harms is the best way to empower folx to live healthful lives.
-    <br/>
-      <br/>
-  <!--  <img class="image" src="Pics/myface.jpg" alt=""/> -->
     </div>
    
   );
